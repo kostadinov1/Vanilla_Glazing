fa2d8a672ecf1bb994d750c059b265e4ec290b4b
kostadinov1 <evgenikostadinov1987@gmail.com>
2023-06-06 03:16:35 +0300

    modified header

--- src/components/Main/Header/Header.js
+++ src/components/Main/Header/Header.js
@@ -2,45 +2,46 @@ import styles from './Header.module.css'
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { NavLink } from "react-router-dom";
+import { HomeFilled, PhoneFilled, ScheduleFilled, ShopFilled, ToolFilled } from '@ant-design/icons';
 
 
 function Header() {
-  return (
-    <div className={`${styles.header}`}>
-        <div className={styles.logo}>
-            <Link to={'/'}>
-                <img src='/logo/dogramaLogoOnly.png'  alt='' className={styles.logo_image} />
-            </Link>
+    return (
+        <div className={`${styles.header}`}>
+            <div className={styles.logo}>
+                <Link to={'/'}>
+                    <img src='/logo/dogramaLogoOnly.png'  alt='' className={styles.logo_image} />
+                </Link>
+            </div>
+            <nav className={styles.ul}>
+                <NavLink to={'/'} 
+                    activeClassName={'active'} 
+                    className={styles.link}> 
+                    <HomeFilled/> Начало
+                </NavLink>
+                <NavLink to={'/projects'} 
+                    activeClassName={'active'} 
+                    className={styles.link}>
+                    <ScheduleFilled/> Проекти
+                </NavLink>
+                <NavLink to={'/products'} 
+                    activeClassName={'active'} 
+                    className={styles.link}>
+                    <ShopFilled/> Продукти
+                </NavLink>
+                <NavLink to={'/services'} 
+                    activeClassName={'active'} 
+                    className={styles.link}>
+                    <ToolFilled/> Услуги
+                </NavLink>
+                <NavLink to={'/contacts'} 
+                    activeClassName={'active'} 
+                    className={styles.link}>
+                    <PhoneFilled/> Контакти
+                </NavLink>
+            </nav>        
         </div>
-        <ul className={styles.ul}>
-
-               <li className={styles.li}>
-                    {/* <NavLink
-                        to="/"
-                        className={`${({ isActive, isPending }) =>
-                            isPending ? "pending" : isActive ? "active" : "link"} ${styles.link}`}
-                        >
-                        Начало
-                    </NavLink> */}
-                    <Link to={'/'} className={styles.link}>Home</Link>
-                </li>
-                <li className={styles.li}>
-                    <Link to={'/projects'} className={styles.link}> Проекти</Link>
-                </li>
-                <li className={styles.li}>
-                    <Link to={'/products'} className={styles.link}> Продукти</Link>
-                </li>
-                <li className={styles.li}>
-                    <Link to={'/services'} className={styles.link}> Услуги</Link>
-                </li>
-                <li className={styles.li}>
-                    <Link to={'/contacts'} className={styles.link}> Контакти</Link>
-                </li>
-        </ul>
-
-        
-    </div>
-  )
-}
+    )
+    }
 
 export default Header
