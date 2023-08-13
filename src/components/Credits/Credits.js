bbab9dad849bf7b70e1f0026fb7ddd1b7e8516e6
kostadinov1 <evgenikostadinov1987@gmail.com>
2023-08-13 17:17:08 +0300

    modified Credits

--- src/components/Credits/Credits.js
+++ src/components/Credits/Credits.js
@@ -24,8 +24,9 @@ const Credits = () => {
                 className={`${styles.credit_img}`}
                 alt='' src='/images/placeholders/measuring-02.jpg' />
                 <div className={`${styles.credit_message_box}`}>
-                Image by <a href="https://www.freepik.com/free-photo/service-maintenance-worker-repairing_20288642.htm#query=construction%20windows%20measuring&position=0&from_view=search&track=ais">Freepik</a>
-                </div>
+                <a href="https://www.freepik.com/free-photo/male-worker-factory_4410596.htm#query=construction%20windows%20measuring&position=48&from_view=search&track=ais">Image by senivpetro</a> on Freepik 
+                
+                 </div>
             </div>
 
             <div className={`${styles.credit_box}`}>
@@ -33,7 +34,7 @@ const Credits = () => {
                 className={`${styles.credit_img}`}
                 alt='' src='/images/placeholders/production.jpg' />
                 <div className={`${styles.credit_message_box}`}>
-                Image by <a href="https://www.freepik.com/free-photo/service-maintenance-worker-repairing_20288642.htm#query=construction%20windows%20measuring&position=0&from_view=search&track=ais">Freepik</a>
+                <a href="https://www.freepik.com/free-photo/male-with-red-shirt-making-window-with-industrial-tools_17650169.htm#query=construction%20windows&position=33&from_view=search&track=ais">Image by wirestock</a> on Freepik
                 </div>
             </div>
 
@@ -43,7 +44,7 @@ const Credits = () => {
                 className={`${styles.credit_img}`}
                 alt='' src='/images/placeholders/production-01.jpeg' />
                 <div className={`${styles.credit_message_box}`}>
-                Image by <a href="https://www.freepik.com/free-photo/service-maintenance-worker-repairing_20288642.htm#query=construction%20windows%20measuring&position=0&from_view=search&track=ais">Freepik</a>
+                <a href="https://www.freepik.com/free-vector/pvc-window-design-production_7437899.htm#query=pvc%20window%20production&position=7&from_view=search&track=ais">Image by macrovector</a> on Freepik
                 </div>
             </div>
 
@@ -52,7 +53,7 @@ const Credits = () => {
                 className={`${styles.credit_img}`}
                 alt='' src='/images/placeholders/contacts.jpg' />
                 <div className={`${styles.credit_message_box}`}>
-                Image by <a href="https://www.freepik.com/free-photo/service-maintenance-worker-repairing_20288642.htm#query=construction%20windows%20measuring&position=0&from_view=search&track=ais">Freepik</a>
+                <a href="https://www.freepik.com/free-photo/young-happy-businesswoman-working-computer-listening-music-headphones-office-her-colleagues-are-working-background_25743765.htm#page=6&query=contact&position=34&from_view=keyword&track=sph">Image by Drazen Zigic</a> on Freepik
                 </div>
             </div>
 
